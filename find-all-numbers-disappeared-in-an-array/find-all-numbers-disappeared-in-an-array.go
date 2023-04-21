46e95b984da761a98ffdf653b3c01c4a83af63a1
anuritto <anur.usmanov@gmail.com>
2023-04-21 20:19:12 +0400

    feat: 448 Find All Numbers Disappeared in an Array

---
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.go
@@ -0,0 +1,21 @@
+package findallnumbersdisappearedinanarray
+
+func findDisappearedNumbers(nums []int) []int {
+	max := len(nums)
+
+	sorted := make([]int, max) // we can use also boolean, true == num exist
+	for i := 0; i < max; i++ {
+		val := nums[i]
+		// right position for N is N-1
+		sorted[val-1] = val
+	}
+
+	notexist := []int{}
+	for i := 0; i < max; i++ {
+		if sorted[i] == 0 { // we know that all nums > 0
+			notexist = append(notexist, i+1) // if notexist[i] is default => num i+1 absent
+		}
+	}
+
+	return notexist
+}
