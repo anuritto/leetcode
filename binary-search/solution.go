c3211171b94ca8f212b9c47e360548f90099e81b
anuritto <anur.usmanov@gmail.com>
2023-04-26 18:21:04 +0400

    feat: binary-search

--- binary-search/solution.go
+++ binary-search/solution.go
@@ -1,36 +1,22 @@
 package binarysearch
 
-const NOT_FOUND = -1
 
 func search(nums []int, target int) int {
-	return searchInHalf(nums, target, 0)
-}
-
-func searchInHalf(nums []int, target, relativeIndex int) int {
-	if len(nums) == 0 {
-		return NOT_FOUND
-	}
-
-	if len(nums) == 1 {
-		if nums[0] == target {
-			return relativeIndex
-		} else {
-			return NOT_FOUND
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		middle := left + ((right - left) / 2)
+		if nums[middle] == target {
+			return middle
 		}
+
+		if nums[middle] > target {
+			right = middle - 1
+		} else {
+			left = middle + 1
+		}
+
 	}
 
-	midInd := len(nums) / 2
-	if nums[midInd] == target {
-		return midInd + relativeIndex
-	}
-
-	if nums[midInd] > target {
-		nums = nums[0:midInd]
-	} else {
-		nums = nums[midInd+1:]
-		relativeIndex += midInd + 1
-	}
-
-	res := searchInHalf(nums, target, relativeIndex)
-	return res
+	return -1
 }
