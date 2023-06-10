74c37826446c89a8467c5aed8584066e9ceb7962
anuritto <anur.usmanov@gmail.com>
2023-06-11 01:31:55 +0400

    fix: twosumiiinputarrayissorted o(n)

--- two-sum-ii-input-array-is-sorted/solution.go
+++ two-sum-ii-input-array-is-sorted/solution.go
@@ -2,18 +2,17 @@ package twosumiiinputarrayissorted
 
 func twoSum(numbers []int, target int) []int {
 
-	i, j := 0, 1
-	numbersLen := len(numbers)
-	for i < numbersLen-1 {
-		for j < numbersLen {
-			if (numbers[i] + numbers[j]) == target {
-				return []int{i + 1, j + 1}
-			}
-			j++
-		}
-		i++
-		j = i + 1
-	}
+	i, j := 0, len(numbers) -1
+	for i < j {
+		sum := numbers[i] + numbers[j]
 
+		if sum == target {
+			break
+		} else if sum > target {
+			j--
+		} else {
+			i++
+		}
+	}
 	return []int{i + 1, j + 1}
 }
