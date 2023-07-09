e513e1daa765065752e6a16afb66b25e179d5193
anuritto <anur.usmanov@gmail.com>
2023-07-09 17:04:23 +0400

    feat: searchinrotatedsortedarray

--- binary-search/search-in-rotated-sorted-array/solution.go
+++ binary-search/search-in-rotated-sorted-array/solution.go
@@ -1,59 +1,33 @@
 package searchinrotatedsortedarray
 
-import "fmt"
 
 func search(nums []int, target int) int {
-	length, k := len(nums), 0
-	fmt.Println("length= ",length)
-	// if k != 0
-	if nums[0] > nums[length-1] {
+	l, r := 0, len(nums) -1
 
-		left, right := 0, length -1
-		for left <= right  {
-			mid := (left + right) /2
-			fmt.Println("left=", left, " right=",right,  " mid=", mid)
+	for l <= r {
 
-			if mid == 0 {
-				k = length- 1
-				break
-			}
-
-			if nums[mid] <= nums[mid-1] {
-				k = length- mid
-				break
-			}
-			if nums[mid] > nums[0] {
-				left = mid +1
-			}
-			if nums[mid] < nums[0] {
-				right = mid -1
-			}
-		}
-	}
-
-	fmt.Println("k=", k)
-	sortedNums := append(nums[length - k:], nums[:length - k]...)
-
-	left, right := 0, length -1
-	for left <= right {
-		mid := (left + right) /2
-		fmt.Println("left=", left, " right=",right,  " mid=", mid)
-		if sortedNums[mid] == target {
-			unsortedInd := mid - k
-			if unsortedInd < 0 {
-				unsortedInd += length
-			}
-			return unsortedInd
-		}
-
-		if sortedNums[mid] > target {
-			right = mid -1
-		}
+		mid := (l + r) /2
 		
-		if sortedNums[mid] < target {
-			left = mid +1
+		if nums[mid] == target {
+			return mid
 		}
+
+		// mid in right part of slice
+		if nums[mid] < nums[l] {
+
+			// target in right part of slice
+			if nums[mid] < target && target <= nums[r] {
+				l = mid +1
+			} else {  // target in left part of slice
+				r = mid -1
+			}
+		} else { // mid in left part of slice
+			if nums[mid] > target && target >= nums[l] { // target in left part
+				r = mid-1
+			} else {
+				l = mid +1
+			}
+		 }
 	}
 	return -1
-
 }
