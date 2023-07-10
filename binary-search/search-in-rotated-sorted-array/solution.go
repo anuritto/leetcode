55b417caf9a70764136f9bc2ed22be844994e9ac
anuritto <anur.usmanov@gmail.com>
2023-07-10 19:08:56 +0400

    style: fmt

--- binary-search/search-in-rotated-sorted-array/solution.go
+++ binary-search/search-in-rotated-sorted-array/solution.go
@@ -1,13 +1,12 @@
 package searchinrotatedsortedarray
 
-
 func search(nums []int, target int) int {
-	l, r := 0, len(nums) -1
+	l, r := 0, len(nums)-1
 
 	for l <= r {
 
-		mid := (l + r) /2
-		
+		mid := (l + r) / 2
+
 		if nums[mid] == target {
 			return mid
 		}
@@ -17,17 +16,17 @@ func search(nums []int, target int) int {
 
 			// target in right part of slice
 			if nums[mid] < target && target <= nums[r] {
-				l = mid +1
-			} else {  // target in left part of slice
-				r = mid -1
+				l = mid + 1
+			} else { // target in left part of slice
+				r = mid - 1
 			}
 		} else { // mid in left part of slice
 			if nums[mid] > target && target >= nums[l] { // target in left part
-				r = mid-1
+				r = mid - 1
 			} else {
-				l = mid +1
+				l = mid + 1
 			}
-		 }
+		}
 	}
 	return -1
 }
